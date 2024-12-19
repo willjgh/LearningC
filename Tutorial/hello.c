2b769cae7ebbb008375a7bfb7618b4f386ab768b
willjgh <williamjgh2000@gmail.com>
2024-12-19 22:07:30 +0000

    Tried out functions with arguments

--- Tutorial/hello.c
+++ Tutorial/hello.c
@@ -2,7 +2,18 @@
 
 #include <stdio.h>
 
-int main(void)
+int plus_one(int n)
 {
+    return n + 1;
+}
+
+void main(void)
+{
+    int i = 10;
+    int j;
+
+    j = plus_one(i);
+
     printf("Hello world\n");
+    printf("%d plus one is %d", i, j);
 }
